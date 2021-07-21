d4dbe58dd8c1a659a83a41e377262d517fd73322
Danielagm2004 <danielagallegom2004@gmail.com>
2021-07-21 16:53:24 -0500

    Initial commit

---
+++ controlador.js
@@ -0,0 +1,9 @@
+// programa que nos ayuda a establecer
+// el costo total de una consola de videojuegos
+// en el exterior
+// compra en dolares
+// pagar un casillero
+// dolares costo dependiendo de la cantidad de productos
+// envio hasta colombia
+// cantidad de productos
+// pagar impuestos de ingreso al pais (lo digita aduanas)
